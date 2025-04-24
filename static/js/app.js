c60b05b3b3fdbee463280e405913291f690e748a
VannIsTrash <vannthemann2@gmail.com>
2025-04-24 08:36:32 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -7,16 +7,32 @@ function renderPost(post) {
     document.getElementById("feed").appendChild(template);
 }
 
-function submitPost() {
+async function submitPost() {
     const message = document.getElementById("postInput").value;
-    console.log("Would post:", message);
-    alert("Tweet submitted (not really yet)");
+    try{ 
+        const response = await fetch("/api/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",  
+            },
+        }body: JSON.stringify({ 
+            username: username,
+            message: message,
+        })
+
+    }
+    catch(error) {
+        console.error("Error submitting post:")
+    }
 }
 
 window.onload = () => {
-    const hardcodedPost = {
-        username: "admin",
-        message: "Welcome to Banterbird! This post is hardcoded.",
-    };
+    try {
+        const response = await fetch("/api/posts"):
+        const posts = await response.json();
+        catch(error)
+        console.error("Error fetching posts:", error):
+    
+    }
     renderPost(hardcodedPost);
 };
