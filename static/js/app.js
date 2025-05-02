e93ee56e4b5dfc6963fde7e8af83160bfbdc0b58
VannIsTrash <vannthemann2@gmail.com>
2025-05-02 08:36:15 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -27,6 +27,7 @@ async function submitPost() {
 }
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 window.onload = () => {
     try {
         const response = await fetch("/api/posts"):
@@ -37,6 +38,18 @@ window.onload = () => {
     }
     renderPost(hardcodedPost);
 };
+=======
+window.onload = async () => {
+    try {
+   const response = await fetch("/api/posts");
+   const posts = await response.json();
+   posts.forEach((post) => renderPost(post));
+}  catch(error) {
+        console.error("Error fetching posts:", error);
+    }
+    };
+    renderPost(hardcodedPost);
+>>>>>>> Stashed changes
 =======
 window.onload = async () => {
     try {
